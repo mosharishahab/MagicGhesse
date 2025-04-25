01bc239ae8e9549d001080d21a13d9a75c05b144
shahab m <128097623+mosharishahab@users.noreply.github.com>
2025-04-25 19:43:08 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
 
 export default defineConfig({
   root: 'client',
@@ -37,7 +38,7 @@ export default defineConfig({
     outDir: '../dist',
     emptyOutDir: true,
     rollupOptions: {
-      input: 'index.html'
+      input: path.resolve(__dirname, 'client/index.html')
     }
   }
 })
