36a5753aac7a2a6c15fe2c14c52f83f7b0349cfa
shahab m <128097623+mosharishahab@users.noreply.github.com>
2025-04-25 17:33:16 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
 
 export default defineConfig({
+  root: 'client',
   plugins: [
     react(),
     VitePWA({
@@ -31,5 +33,9 @@ export default defineConfig({
         ]
       }
     })
-  ]
+  ],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  }
 })
